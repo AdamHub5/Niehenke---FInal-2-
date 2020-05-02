178c36f8aacd8364ca3b92d171c3d8b0b6c2c08c
Adam Niehenke <a.niehenke@setonhill.edu>
2020-05-02 17:35:41 -0400

    Initial Commit

---
+++ Niehenke - FInal/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Niehenke - FInal
+//
+//  Created by Adam Niehenke on 5/2/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Niehenke - FInal/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Niehenke - FInal
+//
+//  Created by Adam Niehenke on 5/2/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
